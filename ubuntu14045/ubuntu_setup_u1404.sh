15ad2d9d55079b743708dc7b3a4873777056c07d
Denz Alovera <denz.alovera@gmail.com>
2017-10-06 21:36:44 +0800

    consolidate scripts into 1

---
+++ ubuntu14045/ubuntu_setup_u1404.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+# basing this script off of:
+# http://howtoubuntu.org/things-to-do-after-installing-ubuntu-14-04-trusty-tahr
+ 
+# install required stuff
+echo "Downloading GetDeb and PlayDeb" &&
+wget http://archive.getdeb.net/install_deb/getdeb-repository_0.1-1~getdeb1_all.deb http://archive.getdeb.net/install_deb/playdeb_0.3-1~getdeb1_all.deb &&
+
+echo "Installing GetDeb" &&
+sudo dpkg -i getdeb-repository_0.1-1~getdeb1_all.deb &&
+
+echo "Installing PlayDeb" &&
+sudo dpkg -i playdeb_0.3-1~getdeb1_all.deb &&
+
+echo "Deleting Downloads" &&
+rm -f getdeb-repository_0.1-1~getdeb1_all.deb &&
+rm -f playdeb_0.3-1~getdeb1_all.deb
+
+add-apt-repository -y ppa:videolan/stable-daily
+add-apt-repository -y ppa:otto-kesselgulasch/gimp
+add-apt-repository -y ppa:gnome3-team/gnome3
+add-apt-repository -y ppa:webupd8team/java
+add-apt-repository -y ppa:webupd8team/y-ppa-manager
+
+echo 'deb http://download.videolan.org/pub/debian/stable/ /' | sudo tee -a /etc/apt/sources.list.d/libdvdcss.list &&
+echo 'deb-src http://download.videolan.org/pub/debian/stable/ /' | sudo tee -a /etc/apt/sources.list.d/libdvdcss.list &&
+wget -O - http://download.videolan.org/pub/debian/videolan-apt.asc|sudo apt-key add -
+
+sudo apt-get update
+sudo apt-get upgrade
+
+# some essential things, apps and wallpaper etc.
+sudo apt-get install synaptic vlc gimp gimp-data gimp-plugin-registry gimp-data-extras y-ppa-manager bleachbit openjdk-7-jre oracle-java8-installer flashplugin-installer unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller libxine1-ffmpeg mencoder flac faac faad sox ffmpeg2theora libmpeg2-4 uudeview libmpeg3-1 mpeg3-utils mpegdemux liba52-dev mpeg2dec vorbis-tools id3v2 mpg321 mpg123 libflac++6 totem-mozilla icedax lame libmad0 libjpeg-progs libdvdcss2 libdvdread4 libdvdnav4 libswscale-extra-2 ubuntu-restricted-extras ubuntu-wallpapers*
+
+
+sudo apt-get update
+sudo apt-get upgrade
+
+# google
+if [[ $(getconf LONG_BIT) = "64" ]]
+then
+	echo "64bit Detected" &&
+	echo "Installing Google Chrome" &&
+	wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb &&
+	sudo dpkg -i google-chrome-stable_current_amd64.deb &&
+	rm -f google-chrome-stable_current_amd64.deb
+else
+	echo "32bit Detected" &&
+	echo "Installing Google Chrome" &&
+	wget https://dl.google.com/linux/direct/google-chrome-stable_current_i386.deb &&
+	sudo dpkg -i google-chrome-stable_current_i386.deb &&
+	rm -f google-chrome-stable_current_i386.deb
+fi
+
+sudo apt-get update
+sudo apt-get upgrade
+
+# clean up installs
+echo "Cleaning Up" &&
+apt-get -f install &&
+apt-get autoremove &&
+apt-get -y autoclean &&
+apt-get -y clean
