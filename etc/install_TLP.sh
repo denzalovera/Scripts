e08f4b9b33701e2a1929cd55ebd4312e3c349ce2
Denz Alovera <denz.alovera@gmail.com>
2018-04-24 19:29:43 +0800

    Mint install scripts

---
+++ etc/install_TLP.sh
@@ -0,0 +1,6 @@
+sudo add-apt-repository ppa:linrunner/tlp
+sudo apt-get update
+sudo apt-get install tlp tlp-rdw
+sudo tlp start
+
+sudo ./laptop-tlp.sh
---
+++ etc/laptop-tlp.sh
@@ -0,0 +1,5 @@
+sudo add-apt-repository ppa:ubuntuhandbook1/apps
+sudo apt-get update
+sudo apt-get install laptop-mode-tools
+
+gksu lmt-config-gui
